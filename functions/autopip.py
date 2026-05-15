0bbdcba42d8dcf807abd679ce042864b18af0bf5
Dimoka113 <133571582+Dimoka113@users.noreply.github.com>
2026-05-15 16:06:16 +0400

    pre 1.23 support installation in env
    Added support for automatic dependency installation when the user uses env

--- functions/autopip.py
+++ functions/autopip.py
@@ -1,13 +1,20 @@
-import subprocess, sys
+import subprocess
+import sys
 from pathlib import Path
+from functions.logger import Logger
 
 
-def install_requirements(self, requirements_path="requirements.txt"):
+def in_venv(): return sys.prefix != sys.base_prefix
+
+
+def install_requirements(self, requirements_path = "requirements.txt", logger: Logger = Logger("pip")):
     req_file = Path(requirements_path)
-    if not req_file.exists():
-        raise FileNotFoundError(f"{requirements_path} не найден")
+    if not req_file.exists(): raise FileNotFoundError(f"{requirements_path} not found")
+    
+    if sys.prefix != sys.base_prefix: logger("found virtual env.")
+    else: logger("System Python is used.")
+    
     try:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", str(req_file)])
-    except subprocess.CalledProcessError as e:
-        self.logger("Ошибка установки зависимостей.")
-        raise e
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "-r" , str(req_file)])
+    except subprocess.CalledProcessError as e: 
+        logger.crit("Dependency installation error."); raise e
